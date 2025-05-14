b81f1fdf001707a1335c57d15a8edfef20102e82
Giorgia Valenti <giorgia.valenti@eds-org.it>
2025-05-14 14:49:33 +0200

    Add ReservationForm based on Reservation model

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('home-class/', views.HomeView.as_view(), name='home_class')
 ]

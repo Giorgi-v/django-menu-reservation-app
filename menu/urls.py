385a21f407ae05a1e46d8df4a349d62e99479601
Giorgia Valenti <giorgia.valenti@eds-org.it>
2025-05-20 14:50:57 +0200

    Add ReservationForm view and template with CSRF handling

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('home-class/', views.HomeView.as_view(), name='home_class')
+    path('home', views.welcome, name='welcome'),
+    path('home-class/', views.HomeView.as_view(), name='home_class'),
+    path('reservation', views.home)
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 
 # Create your views here.
-def home(request):
+def welcome(request):
     return HttpResponse("Welcome to Little Lemon")
 
 class HomeView(View):
     def get(self, request):
         return HttpResponse("Welcome to Little Lemon")
+
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success")
+    
+    return render(request, 'index.html', {'form' : form })
